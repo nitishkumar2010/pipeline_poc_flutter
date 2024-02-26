32e354dab3eafbe8ef55d1001eddfd4b4c8e9b20
Nitish Kumar <nikumar@EX2INDIA.LOCAL>
2024-02-26 17:59:54 +0530

    test

--- integration_test/main_test.dart
+++ integration_test/main_test.dart
@@ -7,7 +7,7 @@ import 'scripts/flow_main.dart';
 void main() {
   final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   binding.framePolicy = LiveTestWidgetsFlutterBindingFramePolicy.fullyLive;
-  testWidgets('Login and create new request', (tester) async {
+  testWidgets('Favorite item', (tester) async {
     app.main();
     await tester.pumpAndSettle(const Duration(seconds: 1));
     //Login
